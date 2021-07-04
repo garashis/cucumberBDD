7671fd8382c9fd024f44f21195c4a54f0cb1f4bc
Ashish Garg <ashgarg@publicisgroupe.net>
2021-07-04 18:59:24 +0100

    Updated Code

--- src/test/java/com/example/cucumberBDD/StepDefinitions.java
+++ src/test/java/com/example/cucumberBDD/StepDefinitions.java
@@ -20,7 +20,7 @@ public class StepDefinitions {
     @Given("^I want to test (.+)$")
     public void i_have_cukes_in_my_belly(String event) throws Throwable {
         Data data = JsonDataReader.INSTANCE.getCustomerByName(event);
-         int status = restTemplate.getForEntity(SERVER_URL + ":" + port + data.getEndPoint(), String.class).getStatusCodeValue();
+        int status = restTemplate.getForEntity(SERVER_URL + ":" + port + data.getEndPoint(), String.class).getStatusCodeValue();
         System.out.println(">>>>>>>>>> " + status);
 
         Assert.assertEquals(data.getStatus(), restTemplate.getForEntity(SERVER_URL + ":" + port + data.getEndPoint(), String.class).getStatusCodeValue());
